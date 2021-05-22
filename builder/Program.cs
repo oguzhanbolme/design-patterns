91e29eaf1c94b618afb52c32faef7ce5218ecdff
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-05-22 18:28:14 +0300

    simple example

---
+++ builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeBuilder employeeBuilder = new EmployeeBuilder();
+            employeeBuilder
+                .setId(1)
+                .setName("Oğuzhan")
+                .setSurname("Bölme")
+                .setDepartment("IT")
+                .setSalary(1999.99);
+
+            Employee employee = employeeBuilder.build();
+
+            Console.WriteLine(employee.id);
+            Console.WriteLine(employee.name);
+            Console.WriteLine(employee.surname);
+            Console.WriteLine(employee.department);
+            Console.WriteLine(employee.salary);
+        }
+    }
+}
