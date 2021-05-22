6d1afbda043b70b1ca772ad538a8c05a16e59b27
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-05-22 18:27:11 +0300

    employee class created

---
+++ builder/Employee.cs
@@ -0,0 +1,8 @@
+public class Employee
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string surname { get; set; }
+    public string department { get; set; }
+    public double salary { get; set; }
+}
