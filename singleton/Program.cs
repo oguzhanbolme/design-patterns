adb24fe33e6c72aa3633d363a6084eb452e05a01
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-05-23 00:44:39 +0300

    simple example

---
+++ singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.getInstance();
+            Singleton singleton2 = Singleton.getInstance();
+            Singleton singleton3 = Singleton.getInstance();
+
+            Console.WriteLine(singleton1.GetHashCode());
+            Console.WriteLine(singleton2.GetHashCode());
+            Console.WriteLine(singleton3.GetHashCode());
+        }
+    }
+}
