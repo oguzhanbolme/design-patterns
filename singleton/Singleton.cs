91cc9bbfe0437cb81e49d1c749fddbe4491adcf4
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-05-23 00:44:08 +0300

    singleton class created

---
+++ singleton/Singleton.cs
@@ -0,0 +1,16 @@
+public class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+}
