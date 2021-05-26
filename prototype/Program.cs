310a07106c42297322c1426702966e7803550c24
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-05-26 23:20:50 +0300

    simple example

---
+++ prototype/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User(){
+                id = 1,
+                username = "ozi",
+                password = "123"
+            };
+
+            User user2 = (User)user1.Clone();
+            user2.id = 2;
+            user2.username = "torres";
+            user2.password = "321";
+
+            Console.WriteLine(user1.GetHashCode());
+            Console.WriteLine(user2.GetHashCode());
+        }
+    }
+}
