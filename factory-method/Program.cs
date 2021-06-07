1b7f8039f95bf1fab53433a28cd36302e1b95f2b
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-06-08 00:33:38 +0300

    simple example

---
+++ factory-method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace factory_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productFactory = new ProductFactory();
+
+            var computer = productFactory.createProduct(ProductTypes.Computer);
+            var phone = productFactory.createProduct(ProductTypes.Phone);
+            var television = productFactory.createProduct(ProductTypes.Television);
+
+            Console.WriteLine(computer.getProductInfo());
+            Console.WriteLine(phone.getProductInfo());
+            Console.WriteLine(television.getProductInfo());
+        }
+    }
+}
