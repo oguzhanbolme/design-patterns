584b5e956222e3a2e60408691e845e0ae81b4091
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-06-08 23:40:53 +0300

    md5 class created

---
+++ adapter/MD5.cs
@@ -0,0 +1,11 @@
+public class MD5 : ICrypto
+{
+    public string decrypt(string encryptedText)
+    {
+        return "Decrypted with MD5";
+    }
+    public string encrypt(string text)
+    {
+        return "Encrypted with MD5";
+    }
+}
