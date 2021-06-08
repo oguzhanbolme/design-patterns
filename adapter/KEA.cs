f96c69a3e4a7c49387a9b5b44eee1f9050963cd7
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-06-08 23:40:44 +0300

    kea class created

---
+++ adapter/KEA.cs
@@ -0,0 +1,11 @@
+public class KEA : ICrypto
+{
+    public string decrypt(string encryptedText)
+    {
+        return "Decrypted with KEA";
+    }
+    public string encrypt(string text)
+    {
+        return "Encrypted with KEA";
+    }
+}
