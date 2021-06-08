6a4e93bfc49b8e82022306434c1542c6143f5969
oguzhanbolme <oguzhanbolme1@gmail.com>
2021-06-08 23:41:00 +0300

    rsa class created

---
+++ adapter/RSA.cs
@@ -0,0 +1,11 @@
+public class RSA
+{
+    public string decodeTheText(string encodedText)
+    {
+        return "Decrypted with RSA";
+    }
+    public string encodeTheText(string text)
+    {
+        return "Encrypted with RSA";
+    }
+}
